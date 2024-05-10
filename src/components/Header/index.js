6b97f3a5c3b4523fcd5a7bb02b4cf95f862de346
Nithindevloper <111164067+Nithindevloper@users.noreply.github.com>
2024-05-10 10:52:08 +0530

    Update Header index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,59 @@
+import {Link, withRouter} from 'react-router-dom'
+import {AiFillHome} from 'react-icons/ai'
+import {BsFillBriefcaseFill} from 'react-icons/bs'
+import {FiLogOut} from 'react-icons/fi'
 import Cookies from 'js-cookie'
-import {withRouter, Link} from 'react-router-dom'
+
 import './index.css'
 
 const Header = props => {
   const onClickLogout = () => {
-    const {history} = props
     Cookies.remove('jwt_token')
+    const {history} = props
     history.replace('/login')
   }
-
   return (
-    <nav className="navbar-container">
-      <div>
+    <nav className="navbar">
+      <div className="nav-container">
         <Link to="/" className="link-item">
           <img
             src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
             alt="website logo"
-            className="website-logo"
+            className="home-website-logo"
           />
         </Link>
-      </div>
-      <ul className="header-list-items">
-        <Link to="/" className="link-item">
-          <li className="home-heading home">Home</li>
-        </Link>
-        <Link to="/jobs" className="link-item">
-          <li className="jon-heading home">Jobs</li>
-        </Link>
-        <li>Contact Us</li>
-      </ul>
-      <div>
-        <button type="button" className="logout-button" onClick={onClickLogout}>
+        <ul className="nav-items">
+          <li>
+            <Link to="/" className="nav-link-lg">
+              Home
+            </Link>
+            <Link to="/" className="nav-link-sm">
+              <AiFillHome className="small-header-icons" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/jobs" className="nav-link-lg">
+              Jobs
+            </Link>
+            <Link to="/jobs" className="nav-link-sm">
+              <BsFillBriefcaseFill className="small-header-icons" />
+            </Link>
+          </li>
+          <li className="logout-btn-list-item-small">
+            <button
+              type="button"
+              className="logout-button-sm"
+              onClick={onClickLogout}
+            >
+              <FiLogOut className="logout-icon-sm" />
+            </button>
+          </li>
+        </ul>
+        <button
+          type="button"
+          className="logout-button-lg"
+          onClick={onClickLogout}
+        >
           Logout
         </button>
       </div>
